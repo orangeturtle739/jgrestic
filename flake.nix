67fdccbd41200e8e0e194916ca59dc93392fe3c6
Jacob Glueck <swimgiraffe435@gmail.com>
2023-01-22 17:46:35 -0800

    Add overlay

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
   description = "A tool for running restic backups";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.dirstamp = {
     url = "github:orangeturtle739/dirstamp";
     inputs.nixpkgs.follows = "nixpkgs";
   };
-  inputs.flake-utils = {
-    url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs.flake-utils = { url = "github:numtide/flake-utils"; };
   inputs.unicmake = {
     url = "github:orangeturtle739/unicmake";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -17,38 +14,48 @@
   # inputs.unicmake.url = "/home/jacob/Documents/MyStuff/projects/unicmake";
 
   outputs = { self, nixpkgs, dirstamp, flake-utils, unicmake }:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
-    (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        pydeps = with pkgs.python3Packages; [ setuptools click ];
-        pybuilddeps = with pkgs.python3Packages; [
-          black
-          flake8
-          isort
-          mypy
-          wrapPython
-          pytest
-        ];
-        jgrestic = pkgs.stdenv.mkDerivation rec {
-          pname = "jgrestic";
-          version =
-            "0.2.2"; # pkgs.lib.removeSuffix "\n" (builtins.readFile ./VERSION);
-          src = self;
-          nativeBuildInputs = pybuilddeps ++ [
-            pkgs.cmake
-            pkgs.ensureNewerSourcesForZipFilesHook
-            pkgs.restic
-            dirstamp.defaultPackage.${system}
+    let
+      withPkgs = base:
+        let
+          pkgs = (base.extend unicmake.overlays.default).extend
+            dirstamp.overlays.default;
+          pydeps = with pkgs.python3Packages; [ setuptools click ];
+          pybuilddeps = with pkgs.python3Packages; [
+            black
+            flake8
+            isort
+            mypy
+            wrapPython
+            pytest
           ];
-          buildInputs = [ unicmake.defaultPackage.${system} ];
-          propagatedBuildInputs = pydeps;
-          postFixup = ''
-            wrapPythonPrograms
-              '';
+          jgrestic = pkgs.stdenv.mkDerivation rec {
+            pname = "jgrestic";
+            version =
+              "0.2.2"; # pkgs.lib.removeSuffix "\n" (builtins.readFile ./VERSION);
+            src = self;
+            nativeBuildInputs = pybuilddeps ++ [
+              pkgs.cmake
+              pkgs.ensureNewerSourcesForZipFilesHook
+              pkgs.restic
+              pkgs.dirstamp
+            ];
+            buildInputs = [ pkgs.unicmake ];
+            propagatedBuildInputs = pydeps;
+            postFixup = ''
+              wrapPythonPrograms
+            '';
+          };
+        in {
+          devShells.default = jgrestic;
+          packages.default = jgrestic;
         };
-      in {
-        devShell = jgrestic;
-        defaultPackage = jgrestic;
-      });
+    in (flake-utils.lib.eachSystem [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+    ] (system: withPkgs (import nixpkgs { inherit system; }))) // {
+      overlays.default = final: prev: {
+        jgrestic = (withPkgs prev).packages.default;
+      };
+    };
 }
